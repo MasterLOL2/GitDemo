939fd784ae50bacbbcad4f7eabc64d11ae0cd7cd
MasterLOL2 <izapotochny78@gmail.com>
2019-02-07 18:57:52 +0200

    Add new file

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Formatting
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("********************");
+            Console.WriteLine("**** GitHubDemo ****");
+            Console.WriteLine("********************");
+            // Main program
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.WriteLine("Hello, World!");
+
+            // Waits until press Enter before shutting down
+            Console.ReadLine();
+        }
+    }
+}
