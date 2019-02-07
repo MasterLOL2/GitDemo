e70381d253bed7a60a2a50ac6fcc7390ea57afb1
MasterLOL2 <izapotochny78@gmail.com>
2019-02-07 19:06:10 +0200

    Add first changes

--- Program.cs
+++ Program.cs
@@ -10,16 +10,16 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             // Formatting
-            Console.BackgroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Yellow;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("********************");
             Console.WriteLine("**** GitHubDemo ****");
             Console.WriteLine("********************");
             // Main program
             Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine();
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, my friends!");
 
             // Waits until press Enter before shutting down
             Console.ReadLine();
